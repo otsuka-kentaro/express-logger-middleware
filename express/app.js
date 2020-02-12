daf7591dba551207c80b408f4e9ea9b685363bf5
otsuka.kentaro <kentaro_otsuka@metaps.com>
2020-02-12 17:44:26 +0900

    add exception handler

--- express/app.js
+++ express/app.js
@@ -3,7 +3,10 @@ const loggingRequest = require('@k-o/express-logger-middleware').loggingRequest
 const app = express()
 const port = 3000
 
-app.use(loggingRequest({ level: 'trace', timeZone: 'Asia/Tokyo' }))
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(loggingRequest({ timeZone: 'Asia/Tokyo' }));
+app.get('/', (_req, res) => res.send('Hello World!'));
+app.get('/exception', (_req, _res) => {
+  throw 'exception!'
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
